faea9cd3f76cf591bc20d62fb5d2971147d096bd
sk <djtestch89@gmail.ru>
2024-03-26 18:00:10 +0500

    created database

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import random
+from datetime import datetime
+from typing import Iterator
+
+import uvicorn
 from fastapi import FastAPI
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse, StreamingResponse
@@ -22,7 +27,7 @@ impoc = [7498, 7498, 7499, 7499, 7500, 7500, 7501, 7501, 7502, 7502, 7502, 7503,
 
 
 def generation():
-    print ('generation')
+    print('generation')
     return ()
 
 def write_db():
@@ -41,11 +46,10 @@ async def index(request: Request) -> Response:
 
 @app.get("/chart-data")
 async def chart_data(request: Request) -> StreamingResponse:
-
-    print('chart')
-    await asyncio.sleep(1)
-    return ()
-
+    response = StreamingResponse(generate_random_data(request), media_type="text/event-stream")
+    response.headers["Cache-Control"] = "no-cache"
+    response.headers["X-Accel-Buffering"] = "no"
+    return response
 
 
 
@@ -63,25 +67,17 @@ async def chart_data(request: Request) -> StreamingResponse:
 #     return templates.TemplateResponse("index.html", {"request": request})
 #
 #
-# async def generate_random_data(request: Request) -> Iterator[str]:
-#     """
-#     Generates random value between 0 and 100
-#
-#     :return: String containing current timestamp (YYYY-mm-dd HH:MM:SS) and randomly generated data.
-#     """
-#     client_ip = request.client.host
-#
-#     logger.info("Client %s connected", client_ip)
-#
-#     while True:
-#         json_data = json.dumps(
-#             {
-#                 "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-#                 "value": random.random() * 100,
-#             }
-#         )
-#         yield f"data:{json_data}\n\n"
-#         await asyncio.sleep(1)
+async def generate_random_data(request: Request) -> Iterator[str]:
+
+    while True:
+        json_data = json.dumps(
+            {
+                "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                "value": random.random() * 100,
+            }
+        )
+        yield f"data:{json_data}\n\n"
+        await asyncio.sleep(1)
 #
 #
 # @application.get("/chart-data")
@@ -90,3 +86,7 @@ async def chart_data(request: Request) -> StreamingResponse:
 #     response.headers["Cache-Control"] = "no-cache"
 #     response.headers["X-Accel-Buffering"] = "no"
 #     return response
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ models/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import MetaData, String, TIMESTAMP, ForeignKey, Table, Column, Integer, JSON,Float
+
+metadata = MetaData()
+
+impoc_val = Table(
+    "impoc_val",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("hardness", Float, nullable=False),
+    Column("impoc", Integer, nullable=False),
+)
